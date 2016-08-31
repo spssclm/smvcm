44b41e845dee546cdedffa5e2e1659402f3bff41
linfeng.yuan <linfeng.yuan@cloudcross.com>
2016-08-31 14:10:44 +0800

    扩展UserController连抄带画的把这个项目算是彻底弄完了。

--- src/main/java/com/cloudcross/anon/controller/UserController.java
+++ src/main/java/com/cloudcross/anon/controller/UserController.java
@@ -1,22 +1,35 @@
 package com.cloudcross.anon.controller;
 
+import java.util.Map;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cloudcross.anon.model.User;
+import com.cloudcross.anon.service.IUserService;
 
 @Controller  
 @RequestMapping("/user")
 public class UserController {
 
+	@Resource
+	private IUserService userService;
+	
 	private static Logger log=LoggerFactory.getLogger(UserController.class);
 	
+	// /user/test?id=1
 	@RequestMapping(value="/test",method=RequestMethod.GET)
 	public String test(HttpServletRequest request,Model model){  
         int userId = Integer.parseInt(request.getParameter("id"));  
@@ -34,4 +47,59 @@ public class UserController {
         model.addAttribute("user", user);  
         return "index";  
     }
+	
+	// /user/showUser?id=1
+	@RequestMapping(value="/showUser",method=RequestMethod.GET)
+	public String toIndex(HttpServletRequest request,Model model){
+		int userId = Integer.parseInt(request.getParameter("id"));
+		System.out.println("userId:"+userId);
+		User user = this.userService.getUserById(userId);
+		log.debug(user.toString());
+		model.addAttribute("user",user);
+		return "showUser";
+	}
+	
+	// /user/showUser2?id=1
+	@RequestMapping(value="/showUser2",method=RequestMethod.GET)
+	public String toIndex2(@RequestParam("id") String id,Model model){
+		int userId = Integer.parseInt(id);
+		System.out.println("userId:"+userId);
+		User user = this.userService.getUserById(userId);
+		log.debug(user.toString());
+		model.addAttribute("user",user);
+		return "showUser";		
+	}
+	
+	// /user/showUser3/{id}
+	@RequestMapping(value="/showUser3/{id}",method=RequestMethod.GET)
+	public String toIndex3(@PathVariable("id")String id,Map<String, Object> model){
+		int userId = Integer.parseInt(id);
+		System.out.println("userId:"+userId);
+		User user = this.userService.getUserById(userId);
+		log.debug(user.toString());
+		model.put("user",user);
+		return "showUser";		
+	}
+	
+	// /user/{id}
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public @ResponseBody User getUserInJson(@PathVariable String id,Map<String, Object> model){
+		int userId = Integer.parseInt(id);
+		System.out.println("userId:"+userId);
+		User user = this.userService.getUserById(userId);
+		log.debug(user.toString());
+		return user;
+	}
+	
+	// /user/{id}
+	@RequestMapping(value="/jsontype/{id}",method=RequestMethod.GET)
+	public ResponseEntity<User> getUserInJson2(@PathVariable String id,Map<String, Object> model){
+		int userId = Integer.parseInt(id);
+		System.out.println("userId:"+userId);
+		User user = this.userService.getUserById(userId);
+		log.debug(user.toString());
+		return new ResponseEntity<User>(user,HttpStatus.OK);
+	}
+	
+	
 }
